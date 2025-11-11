b96c529ed39b695785fba51bfeb47efa309f9ae7
rodri <daw202717@estudianes.salesianasnsp.es>
2025-11-11 08:59:00 +0100

    Proyecto Calculadora.Funcion sumar

---
+++ Calculadora/src/pruebaGIT/Calcualdora.java
@@ -0,0 +1,15 @@
+package pruebaGIT;
+
+public class Calcualdora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1=4, num2=7;
+System.out.println("Calculadora basica");
+int suma=Sumar(num1, num2);
+System.out.println("Suma "+suma);
+	}
+public static int Sumar(int n1, int n2) {
+	return n1+n2;
+}
+}
