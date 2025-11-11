69d7e3ab13364f51ae7e4309bf3caff6c08fb7a4
rodri <daw202717@estudianes.salesianasnsp.es>
2025-11-11 09:08:39 +0100

    Proyecto Calculadora.Funcion Restar

--- Calculadora/src/pruebaGIT/Calcualdora.java
+++ Calculadora/src/pruebaGIT/Calcualdora.java
@@ -8,8 +8,13 @@ int num1=4, num2=7;
 System.out.println("Calculadora basica");
 int suma=Sumar(num1, num2);
 System.out.println("Suma "+suma);
+int resta=Restar(num1, num2);
+System.out.println("Resta "+resta);
 	}
 public static int Sumar(int n1, int n2) {
 	return n1+n2;
+}
+public static int Restar(int n1, int n2) {
+	return n1-n2;
 }
 }
